fd14c626d664dccc962fa469ad907ef8ff5eba1b
Feishi Wang <feishiwang2001@gmail.com>
2025-07-12 15:02:12 -0700

    [add] training script for dreamerv1 and v3

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
+########################
+## PPO
+########################
+
 ## DMControl Tasks
 python ppo.py --env-id=dm_control/cartpole-balance-v0 --seed 2 --num-envs=4 --lr=1e-3 --num_steps=2048 --total_timesteps=1000000
 
@@ -6,3 +10,19 @@ python ppo.py --env-id=isaacgymenv/Cartpole --seed 2 --num-envs=512 --lr=3e-4 --
 
 ## IsaacLab Tasks
 python ppo.py --env-id=isaaclab/Isaac-Reach-Franka-v0 --seed 1 --num-envs=32 --lr=1e-3 --num_steps=360 --total_timesteps=3000000
+
+
+########################
+## DreamerV1
+########################
+
+## DMControl Tasks
+python dm1.py --env-id dm_control/walker-walk-v0
+
+
+########################
+## DreamerV3
+########################
+
+## DMControl Tasks
+python dm3.py --env-id dm_control/walker-walk-v0
