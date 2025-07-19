813aef9367796967dd8251bbf762281db66a7baf
Feishi Wang <feishiwang2001@gmail.com>
2025-07-18 22:26:38 -0700

    fix bug that replay buffer should guarentee sampled data are in sequence (but we still ignore the termination)

--- dm3_sh.py
+++ dm3_sh.py
@@ -62,19 +62,25 @@ def enable_deterministic_run():
 
 class ReplayBuffer:
     def __init__(
-        self, observation_shape: Sequence[int], action_size: int, device: str | torch.device, capacity: int = 5000000
+        self,
+        observation_shape: Sequence[int],
+        action_size: int,
+        device: str | torch.device,
+        num_envs: int = 1,
+        capacity: int = 5000000,
     ):
         self.device = device
+        self.num_envs = num_envs
         self.capacity = capacity
 
         state_type = np.uint8 if len(observation_shape) < 3 else np.float32
 
-        self.observation = np.empty((self.capacity, *observation_shape), dtype=state_type)
-        self.next_observation = np.empty((self.capacity, *observation_shape), dtype=state_type)
-        self.action = np.empty((self.capacity, action_size), dtype=np.float32)
-        self.reward = np.empty((self.capacity, 1), dtype=np.float32)
-        self.done = np.empty((self.capacity, 1), dtype=np.float32)
-        self.terminated = np.empty((self.capacity, 1), dtype=np.float32)
+        self.observation = np.empty((self.capacity, self.num_envs, *observation_shape), dtype=state_type)
+        self.next_observation = np.empty((self.capacity, self.num_envs, *observation_shape), dtype=state_type)
+        self.action = np.empty((self.capacity, self.num_envs, action_size), dtype=np.float32)
+        self.reward = np.empty((self.capacity, self.num_envs, 1), dtype=np.float32)
+        self.done = np.empty((self.capacity, self.num_envs, 1), dtype=np.float32)
+        self.terminated = np.empty((self.capacity, self.num_envs, 1), dtype=np.float32)
 
         self.buffer_index = 0
         self.full = False
@@ -91,21 +97,20 @@ class ReplayBuffer:
         done: Tensor,
         terminated: Tensor,
     ):
-        B = observation.shape[0]
-        indices = (self.buffer_index + np.arange(B)) % self.capacity
-        self.observation[indices] = observation.detach().cpu().numpy()
-        self.action[indices] = action.detach().cpu().numpy()
-        self.reward[indices] = reward.unsqueeze(-1).detach().cpu().numpy()
-        self.next_observation[indices] = next_observation.detach().cpu().numpy()
-        self.done[indices] = done.unsqueeze(-1).detach().cpu().numpy()
-        self.terminated[indices] = terminated.unsqueeze(-1).detach().cpu().numpy()
+        self.observation[self.buffer_index] = observation.detach().cpu().numpy()
+        self.action[self.buffer_index] = action.detach().cpu().numpy()
+        self.reward[self.buffer_index] = reward.unsqueeze(-1).detach().cpu().numpy()
+        self.next_observation[self.buffer_index] = next_observation.detach().cpu().numpy()
+        self.done[self.buffer_index] = done.unsqueeze(-1).detach().cpu().numpy()
+        self.terminated[self.buffer_index] = terminated.unsqueeze(-1).detach().cpu().numpy()
 
-        self.full = self.full or self.buffer_index + B >= self.capacity
-        self.buffer_index = (self.buffer_index + B) % self.capacity
+        self.buffer_index = (self.buffer_index + 1) % self.capacity
+        self.full = self.full or self.buffer_index == 0
 
     def sample(self, batch_size, chunk_size) -> dict[str, Tensor]:
         """
-        (batch_size, chunk_size, input_size)
+        Sample elements from the replay buffer in a sequential manner, without considering the episode
+        boundaries.
         """
         last_filled_index = self.buffer_index - chunk_size + 1
         assert self.full or (last_filled_index > batch_size), "too short dataset or too long chunk_size"
@@ -115,14 +120,18 @@ class ReplayBuffer:
         chunk_length = np.arange(chunk_size).reshape(1, -1)
 
         sample_index = (sample_index + chunk_length) % self.capacity
+        env_index = np.random.randint(0, self.num_envs, batch_size)
+        flattened_index = sample_index * self.num_envs + env_index[:, None]
 
-        observation = torch.as_tensor(self.observation[sample_index], device=self.device).float()
-        next_observation = torch.as_tensor(self.next_observation[sample_index], device=self.device).float()
+        def flatten(x: np.ndarray) -> np.ndarray:
+            return x.reshape(-1, *x.shape[2:])
 
-        action = torch.as_tensor(self.action[sample_index], device=self.device)
-        reward = torch.as_tensor(self.reward[sample_index], device=self.device)
-        done = torch.as_tensor(self.done[sample_index], device=self.device)
-        terminated = torch.as_tensor(self.terminated[sample_index], device=self.device)
+        observation = torch.as_tensor(flatten(self.observation)[flattened_index], device=self.device).float()
+        next_observation = torch.as_tensor(flatten(self.next_observation)[flattened_index], device=self.device).float()
+        action = torch.as_tensor(flatten(self.action)[flattened_index], device=self.device)
+        reward = torch.as_tensor(flatten(self.reward)[flattened_index], device=self.device)
+        done = torch.as_tensor(flatten(self.done)[flattened_index], device=self.device)
+        terminated = torch.as_tensor(flatten(self.terminated)[flattened_index], device=self.device)
 
         sample = {
             "observation": observation,
@@ -910,7 +919,13 @@ writer.add_text(
     "hyperparameters",
     "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
 )
-buffer = ReplayBuffer(envs.single_observation_space.shape, envs.single_action_space.shape[0], device, capacity=500_000)
+buffer = ReplayBuffer(
+    envs.single_observation_space.shape,
+    envs.single_action_space.shape[0],
+    device,
+    num_envs=args.num_envs,
+    capacity=500_000,
+)
 
 ## networks
 encoder = Encoder().to(device)
