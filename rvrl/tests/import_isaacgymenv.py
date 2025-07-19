17e30fa148c4a9578cbcb11eb014af2f1385bac5
Feishi Wang <feishiwang2001@gmail.com>
2025-07-19 00:09:49 -0700

    don't use rootutils

--- rvrl/tests/import_isaacgymenv.py
+++ rvrl/tests/import_isaacgymenv.py
@@ -1,6 +1,3 @@
-import rootutils
-
-rootutils.setup_root(__file__, pythonpath=True)
 from rvrl.envs.isaacgym_env import IsaacGymEnv
 
 env = IsaacGymEnv(task_name="Cartpole", num_envs=1, seed=0, device="cuda")
